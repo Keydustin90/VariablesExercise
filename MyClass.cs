d3c1cf89ecec0d9f1c9cae361f62e70dfa479f91
Dustin Key <Keydustin90@gmail.com>
2022-03-01 10:18:31 -0600

    finished Variables

---
+++ MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace VariablesExercise
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
+string dogName = "Morgang"
+int dogAge = 3;
+char nickName = 'Mo'
+bool lovesCuddles = true;
+double tennisBalls = 10;
+decimal Weight = 50.5m;
+
+Console.WriteLine($"My dog's name is {dogName}, She is {dogAge} years old and has {tennisBalls} Tennis balls and she loves to cuddles {lovesCuddles}. She will also awnser to {nickName} and she weight {Weight}.")
